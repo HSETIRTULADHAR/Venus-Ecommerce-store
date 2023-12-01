81f1a055a844b9b5d50778c61cc909e1b8613c89
HSETIRTULADHAR <52373550+HSETIRTULADHAR@users.noreply.github.com>
2023-12-02 00:21:49 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   },
   env: {
     API_URL: "https://venus-ecommerce-store-1ldc9zz6i-hsetirtuladhar.vercel.app/",
-    DB_URI: "mongodb+srv://ecommerce:NePaL530351@cluster0.0mporsl.mongodb.net/?retryWrites=true&w=majority/",
+    DB_URI: "mongodb+srv://ecommerce:NePaL530351@buyitnow.0e0exre.mongodb.net/?retryWrites=true&w=majority",
   },
   images: {
     domains: ["res.cloudinary.com"],
