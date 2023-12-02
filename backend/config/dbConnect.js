5e9fb1b266452baa79970288eff4626f58ba48eb
HSETIRTULADHAR <52373550+HSETIRTULADHAR@users.noreply.github.com>
2023-12-02 18:08:40 +0545

    Update dbConnect.js

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -1,12 +1,28 @@
 import mongoose from "mongoose";
+import Product from "../models/product";
 
 const dbConnect = () => {
+
   if (mongoose.connection.readyState >= 1) {
+    console.log("Value of ready state is more than 1", mongoose.connection.readyState);
     return;
   }
 
+
   mongoose.set("strictQuery", false);
-  mongoose.connect(process.env.DB_URI);
+  mongoose.connect("mongodb+srv://ecommerce:NePaL530351@cluster0.0mporsl.mongodb.net/buyitnow?retryWrites=true&w=majority")
+          .then(
+            () => {
+              console.log("Mongodb database connected")
+              Product.find().then((item)=> {
+                  console.log(item);
+              })
+            }
+          )
+
+          .catch((err)=> {
+                console.log("Error occured = ", err)
+          })
 };
 
 export default dbConnect;
