a11f9cae92f02292054e15614a52edc5852aceb8
HSETIRTULADHAR <52373550+HSETIRTULADHAR@users.noreply.github.com>
2023-12-04 20:47:18 +0545

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,10 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  env: {
-    API_URL: "https://w29qbvkj-3000.asse.devtunnels.ms/",
-    DB_URI: "mongodb+srv://ecommerce:NePaL530351@cluster0.0mporsl.mongodb.net/buyitnow?retryWrites=true&w=majority",
-  },
   images: {
     domains: ["res.cloudinary.com"],
   },
